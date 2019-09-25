756d89d604ea45589418f4a94990151ec85b0d59
idhoramgg <ridhoabdulmajid@gmail.com>
2019-09-25 14:17:03 +0700

    cloud

--- config/database.js
+++ config/database.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
-const {MONGOOSE_DB} = require('./envir')
+const {MONGOOSE_DB, MONGOOSE_CONNECTION} = require('./envir')
 
-const MONGOOSE_CONNECTION = process.env.MONGOOSE_CONNECTION || `mongodb://localhost:27017/${MONGOOSE_DB}`;
+const CONNECTION = MONGOOSE_CONNECTION || `mongodb://localhost:27017/${MONGOOSE_DB}`;
 
-mongoose.connect(MONGOOSE_CONNECTION, {
+mongoose.connect(CONNECTION, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
--- config/envir.js
+++ config/envir.js
@@ -2,5 +2,6 @@ require('dotenv').config()
 
 module.exports = {
   MONGOOSE_DB : process.env.MONGOOSE_DB,
-  PORT: process.env.PORT
+  PORT: process.env.PORT,
+  MONGOOSE_CONNECTION: process.env.MONGOOSE_CONNECTION
 };
