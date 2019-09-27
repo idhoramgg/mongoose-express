b50e64d6945e52c3319b87539fd4625d1701049f
idhoramgg <ridhoabdulmajid@gmail.com>
2019-09-27 14:46:26 +0700

    route

--- routes/user.js
+++ routes/user.js
@@ -20,7 +20,7 @@ router.post('/register', authentication.tokenValid, addUser)
 router.post('/user-image', upload.single('files'), uploadImage);
 router.post('/login', login)
 router.get('/user/:userId', showDataById)
-router.get('/user', authentication.tokenValid, showAll)
+router.get('/user', showAll)
 router.delete('/user/:userId', deleteUser)
 router.put('/user/:userId', updateUser)
 
