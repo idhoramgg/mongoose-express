f213da701a448c053049800dc457a217ee3ea172
idhoramgg <31439757+idhoramgg@users.noreply.github.com>
2022-01-13 11:31:15 +0700

    Update comment.js

--- routes/comment.js
+++ routes/comment.js
@@ -3,12 +3,14 @@ const express = require('express')
 const router = express.Router()
 
 const {
-  addComment
+  addComment,
+  getComment
 } = require('../controllers/comment')
 
 
 
 
 router.post('/',  addComment)
+router.get('/', getComment)
 
 module.exports = router;
